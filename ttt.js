fe2a077f6af91a8c93b58f8276f332ff12925cf9
ecarrell <ecarrell@gmail.com>
2015-02-01 17:20:38 -0600

    Added red,blue,empty % sliders

--- ttt.js
+++ ttt.js
@@ -20,25 +20,42 @@ table.SliderChange = function (){
 	$('#sizeNumber').html($('#slider').val());
 }
 
+table.randomNum = function(){
+	return Math.floor((Math.random() * 3) + 1);
+}
+
 //This function is called initially and every time the user changes
 //the slider's value
 table.buildTable = function(rowCol) {
 
 	var row = document.getElementsByTagName('table')[0];
+	
+	//initialize variables
+	var col;
+	var td;
+	var tr;
+	
+	//clear table
 	row.innerHTML = '';
 		
 	//this loop runs through the rows
 	for(var i = 0; i < rowCol; i++) {
-		var tr = document.createElement('TR');
+		tr = document.createElement('TR');
 		row.appendChild(tr);
 		
 		//this loop runs through the columns
 		for(var j = 0; j < rowCol; j++) {
-			var col = document.getElementsByTagName('tr')[i];
-			var td = document.createElement('TD');
+			col = document.getElementsByTagName('tr')[i];
+			td = document.createElement('TD');
 			col.appendChild(td);
 		}
 	}
 };
 
 $(document).ready(table.setUp);
+
+/* Add to red,blue,empty % slider function 
+		(how to dynamically change slider max attr)
+		
+$('#slider').attr('min', A_Route[0]);
+$('#slider').attr('max', A_Route[3]); */
