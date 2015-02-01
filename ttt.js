802982db3558b335e61195b0c6580ae23dcd319c
ecarrell <ecarrell@users.noreply.github.com>
2015-02-01 14:59:06 -0600

    Add table size edit function

--- ttt.js
+++ ttt.js
@@ -1,8 +1,24 @@
 var table = {
 	
 	//Initial size of table
-	tableCol: 30,
-	tableRow: 30
+	tableRow: 30,
+	tableCol: 30
+	
 	
 	
 };
+
+//This function is called initially and everytime the user changes
+//the size slider's value
+table.resizeTable = function() {
+	
+	//this loop runs through the rows
+	for (var r = 0; r < tableRow; r++) {
+		
+		//this loop runs through the columns
+		for (var c = 0; r < tableCol; c++) {
+			
+		}
+		
+	}
+}
